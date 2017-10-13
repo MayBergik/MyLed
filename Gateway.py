b88fe85f55b8f6e974ab0280f916f305f7b7e415
MayBergik <maybergik@gmail.com>
2017-10-13 11:30:42 +0300

    Gateway.py

--- Gateway.py
+++ Gateway.py
@@ -3,6 +3,7 @@ import os
 import logging
 from flask import request
 from flask import Flask
+"""
 import paho.mqtt.client as paho
 
 MQTT_HOST = os.environ.get("MQTT_HOST", '')
@@ -15,7 +16,7 @@ client.username_pw_set(MQTT_USER, MQTT_PWD)
 client.connect(MQTT_HOST, MQTT_PORT)
 client.publish("topic/test", "My message")
 client.disconnect()
-
+"""
 app = Flask(__name__)
 
 #Default
@@ -28,18 +29,25 @@ def helloWorld():
 @app.route('/webhook', methods=['GET'])
 def verify():
     """webhook api"""
+    logging.warning("verify()")
     return request.args.get('hub.challenge')
+
+#Post request
+@app.route('/', methods=['POST'])
+def postman():
+    jsonDictionary = request.get_json()
+    logging.warning(jsonDictionary["name"])
+    return "post"
 
 #Post request
 @app.route('/webhook', methods=['POST'])
 def facebookWebHook():
-    jsonDictionary = request.get_json()
-    #logging.warning(jsonDictionary["name"])
-    #return "Post!"
-
+    #jsonDictionary = request.get_json()
+    logging.warning("facebookWebHook()")
     #if jsonDictionary['entry'][0]['changes'][0]['value']['item'] == 'like':
 
-    logging.warning('Handled webhook request ' + str(jsonDictionary))
-    return 'post'
+    #logging.warning('Handled webhook request ' + str(jsonDictionary))
+    return ""
+
 
 app.run(host='0.0.0.0', port=int(os.environ.get("PORT", 5000)))
